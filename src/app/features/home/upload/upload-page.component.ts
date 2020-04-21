d5695b90a3c2cd8679a4f55d310288c0a1455b0a
Padmanabhan <pakosal1@in.ibm.com>
2020-04-22 01:27:03 +0530

    - #86 Supply upload to follow SCBN csv format - Supplier id in CSV to match with selected supplier id from dropdown, - Display an error message if above condition fails - Removed the this.suppliers.combox reference as it got removed from HTML

--- src/app/features/home/upload/upload-page.component.ts
+++ src/app/features/home/upload/upload-page.component.ts
@@ -24,9 +24,18 @@ export class UploadPageComponent implements OnInit {
     'UPLOAD.PRODUCTCLASS': '',
     'UPLOAD.MODEL_NUMBER': '',
     'UPLOAD.SHIP_BY_DATE': '',
-    'UPLOAD.SHIP_NODE': ''
+    'UPLOAD.SHIP_NODE': '',
+    'UPLOAD.SUPPLIERID': '',
+    'UPLOAD.CUSTOMERID': '',
+    'UPLOAD.LOCATIONID': '',
+    'UPLOAD.ITEMID': '',
+    'UPLOAD.AVAILABLEFROM': '',
+    'UPLOAD.UPLOAD_CUSTOM_ERROR_MSG': ''
+
   };
-  private readonly reqCols = ['modelnumber','quantity','shipbydate','shipnode']; 
+
+  private readonly reqCols = ['supplierid', 'customerid', 'locationid', 'itemid', 'productclass', 'quantity', 'uom', 'availablefrom'];
+
   private readonly columnNames: string[] = [];
   private pages = [];
   private pgLen = BucTableModel.DEFAULT_PAGE_LEN;
@@ -52,8 +61,8 @@ export class UploadPageComponent implements OnInit {
 
   onUpload(e) {
     this._clearTable();
-    
-   
+
+
     const f = e.target.files[0];
     const r = new FileReader();
     r.onload = () => this._parseFile(r);
@@ -77,7 +86,7 @@ export class UploadPageComponent implements OnInit {
     await this._initTranslations();
     this._initTable();
     await this._initUserDataAndFetchAllSuppliers();
-    if (this.supplierList.length == 1) {
+    if (this.supplierList.length === 1) {
       this.onSupplier({ item: this.supplierList[0] });
     }
   }
@@ -92,11 +101,14 @@ export class UploadPageComponent implements OnInit {
     // note these headers are literals, hence not translatable
     this.model.header = [
       [
-        new TableHeaderItem({ data: this.nlsMap['UPLOAD.MODEL_NUMBER'] }),
+        new TableHeaderItem({ data: this.nlsMap['UPLOAD.SUPPLIERID'] }),
+        new TableHeaderItem({ data: this.nlsMap['UPLOAD.CUSTOMERID'] }),
+        new TableHeaderItem({ data: this.nlsMap['UPLOAD.LOCATIONID'] }),
+        new TableHeaderItem({ data: this.nlsMap['UPLOAD.ITEMID'] }),
+        new TableHeaderItem({ data: this.nlsMap['UPLOAD.PRODUCTCLASS'] }),
         new TableHeaderItem({ data: this.nlsMap['UPLOAD.QUANTITY'] }),
-        new TableHeaderItem({ data: this.nlsMap['UPLOAD.SHIP_BY_DATE'] }),
-        new TableHeaderItem({ data: this.nlsMap['UPLOAD.SHIP_NODE'] }),
-        new TableHeaderItem({ data: this.nlsMap['UPLOAD.UOM'] })
+        new TableHeaderItem({ data: this.nlsMap['UPLOAD.UOM'] }),
+        new TableHeaderItem({ data: this.nlsMap['UPLOAD.AVAILABLEFROM'] })
       ]
     ];
     this.model.data = [];
@@ -148,20 +160,24 @@ export class UploadPageComponent implements OnInit {
     }
 
     if (rc) {
-      this.columnNames.push(... columns);
+      this.columnNames.push(...columns);
     }
 
     return rc;
   }
 
+
   private _loadTable(records) {
     const data = records.map(v => {
       return [
-        { data: v.modelnumber },
+        { data: v.supplierid },
+        { data: v.customerid },
+        { data: v.locationid || '' },
+        { data: v.itemid },
+        { data: v.productclass || '' },
         { data: v.quantity },
-        { data: v.shipbydate || '' },
-        { data: v.shipnode },
-        { data: v.unitofmeasure || 'UNIT' }
+        { data: v.uom || 'UNIT' },
+        { data: v.availablefrom },
       ];
     });
     this.model.totalDataLength = records.length;
@@ -178,31 +194,41 @@ export class UploadPageComponent implements OnInit {
     };
     let adj: AdjustSupply;
 
-    //prefix supplier id:: to itemId & shipNode
+
     records.forEach(v => {
-      adj = {
-        itemId: `${this.supplier}::${v.modelnumber}`,
-        shipNode:`${this.supplier}::${v.shipnode}`,
-        changedQuantity: v.quantity,
-        type: 'ONHAND',
-        unitOfMeasure: v.unitofmeasure || 'UNIT',
-        eta: v.shipbydate || '',
-        shipByDate: v.shipbydate || '',
-      };
-      supplies.push(adj);
+      if (this.supplier === v.supplierid) {
+        console.log('Set the request body', this.supplier, v.supplierid);
+        adj = {
+          itemId: `${this.supplier}::${v.itemid}`,
+          shipNode: `${this.supplier}::${v.locationid}`,
+          quantity: v.quantity,
+          productClass: v.productclass || 'NEW',
+          type: ((new Date(v.availablefrom) > new Date()) ? 'PO_PLACED' : 'ONHAND'),
+          unitOfMeasure: v.uom || 'UNIT',
+          eta: v.availablefrom || '',
+          shipByDate: v.availablefrom || '',
+        };
+        supplies.push(adj);
+      } else {
+        this._showError('', this.nlsMap['UPLOAD.UPLOAD_CUSTOM_ERROR_MSG']);
+      }
     });
 
-    try {
-      console.log('_adjustSupply call to putByTenantIdV1Supplies : params', params)
-      // await this.suppSvc.postByTenantIdV1Supplies(params).toPromise();
-      await this.suppSvc.putByTenantIdV1Supplies(params).toPromise();
-      this._showSuccess('', this.nlsMap['UPLOAD.LABEL_uploadSuccessful']);
-      this._loadTable(records);
-      this.onSelectPage(1);
-    } catch (r) {
-      const error = r.error ? r.error.error_message : r.message;
-      this._showError('', error);
+
+    if (supplies.length > 0) {
+      try {
+        console.log('_adjustSupply call to putByTenantIdV1Supplies : params', params);
+        // await this.suppSvc.postByTenantIdV1Supplies(params).toPromise();
+        await this.suppSvc.putByTenantIdV1Supplies(params).toPromise();
+        this._showSuccess('', this.nlsMap['UPLOAD.LABEL_uploadSuccessful']);
+        this._loadTable(records);
+        this.onSelectPage(1);
+      } catch (r) {
+        const error = r.error ? r.error.error_message : r.message;
+        this._showError('', error);
+      }
     }
+
   }
   private _showSuccess(title, msg) {
     this._showNotification('success', title, msg, true);
@@ -228,20 +254,20 @@ export class UploadPageComponent implements OnInit {
     };
 
     this.user = await this.s4sSvc.getUserInfo().toPromise();
-    console.log('S4S response - _initUserDataAndFetchAllSuppliers - getUserInfo',  this.user);
+    console.log('S4S response - _initUserDataAndFetchAllSuppliers - getUserInfo', this.user);
     const obs = this.user.sellers.map(supplierId => this.s4sSvc.getContactDetailsOfSelectedSupplier({ supplierId }));
     const suppliers = await forkJoin(obs).toPromise();
-    console.log('S4S response - _initUserDataAndFetchAllSuppliers - getContactDetailsOfSelectedSupplier combined',  suppliers);
+    console.log('S4S response - _initUserDataAndFetchAllSuppliers - getContactDetailsOfSelectedSupplier combined', suppliers);
     this.supplierList = suppliers.map(s => ({ content: `${s.description} (${s.supplier_id})`, id: s.supplier_id }));
-    console.log('Model - _initUserDataAndFetchAllSuppliers S4S supplierList ', this.supplierList );
-    
+    console.log('Model - _initUserDataAndFetchAllSuppliers S4S supplierList ', this.supplierList);
+
   }
 
   onSupplier(e) {
     const s = e.item ? e.item.id : undefined;
     if (s && s !== this.supplier) {
       this.supplier = s;
-    }  
+    }
   }
 
 }
