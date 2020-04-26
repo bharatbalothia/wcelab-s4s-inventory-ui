894332894542f56fde6738db4b82bc4144a2483b
Padmanabhan <pakosal1@in.ibm.com>
2020-04-26 23:21:58 +0530

    # 122 Fixed - continuos spinning when s4s returns empty result set - environment file - updated package.json with --configuraion=production. This will pick envrionement.prod.ts file record. If we didnt pass this value, then environment.ts file will be picked at run. - few lint fix

--- src/app/features/home/shared/services/inventory-distribution.service.ts
+++ src/app/features/home/shared/services/inventory-distribution.service.ts
@@ -18,13 +18,13 @@ export class InventoryDistributionService {
   constructor(
     private dgSvc: DistributionGroupService,
     private s4sSvc: S4SSearchService,
-    private shipNodeSvc : ShipNodeService
+    private shipNodeSvc: ShipNodeService
   ) { }
 
   public getDistributionRuleDetails(dgId: string): Observable<any> {
-      const tenId = BucSvcAngularStaticAppInfoFacadeUtil.getInventoryTenantId();
-      return this.dgSvc.getByTenantIdV1ConfigurationDistributionGroupsByDistributionGroupId(
-        { tenantId: tenId, distributionGroupId: dgId})
+    const tenId = BucSvcAngularStaticAppInfoFacadeUtil.getInventoryTenantId();
+    return this.dgSvc.getByTenantIdV1ConfigurationDistributionGroupsByDistributionGroupId(
+      { tenantId: tenId, distributionGroupId: dgId })
       .pipe(
         map(r => r.shipNodes)
       );
@@ -33,55 +33,64 @@ export class InventoryDistributionService {
   public getDistributionRuleListByPage(): Observable<DistributionRuleListApiResponse> {
     const tenantId = BucSvcAngularStaticAppInfoFacadeUtil.getInventoryTenantId();
     return this.dgSvc.getByTenantIdV1ConfigurationDistributionGroups({ tenantId })
-    .pipe(
-      map(r => ({ distributionGroups: r.map(dg => ({ dgId: dg.distributionGroupId, dgName: dg.distributionGroupId })) }))
-    );
+      .pipe(
+        map(r => ({ distributionGroups: r.map(dg => ({ dgId: dg.distributionGroupId, dgName: dg.distributionGroupId })) }))
+      );
   }
 
   public getProducts(): Observable<any> {
-    return this.s4sSvc.getProducts(  {} );
+    return this.s4sSvc.getProducts({})
+    .pipe(catchError((err) => observableOf([])));
   }
 
   public getEntitledProductsBySupplierIds(supplierIds: string[]): Observable<any> {
     const reqPayLoad = { supplier_ids: supplierIds };
-    return this.s4sSvc.getEntitledProductsBySupplierIds(  { body: reqPayLoad} ) ;
+    return this.s4sSvc.getEntitledProductsBySupplierIds({ body: reqPayLoad })
+    .pipe(catchError((err) => observableOf([])));
   }
 
   public getAllCategories(): Observable<any> {
-    return this.s4sSvc.getAllCategories({});
+    return this.s4sSvc.getAllCategories({})
+    .pipe(catchError((err) => observableOf([])));
   }
-  
 
-  public getAllProductsByCategoryId(selectedCategoryId : string): Observable<any> {
-    return this.s4sSvc.getAllProductsByCategoryId({categoryId : selectedCategoryId});
+
+  public getAllProductsByCategoryId(selectedCategoryId: string): Observable<any> {
+    return this.s4sSvc.getAllProductsByCategoryId({ categoryId: selectedCategoryId })
+    .pipe(catchError((err) => observableOf([])));
   }
 
   public fetchAllSuppliers(): Observable<any> {
-    return this.s4sSvc.fetchAllSuppliers({} );
+    return this.s4sSvc.fetchAllSuppliers({})
+    .pipe(catchError((err) => observableOf([])));
   }
 
   public fetchProductList(productIds: string[]): Observable<any> {
     const tenantId = BucSvcAngularStaticAppInfoFacadeUtil.getInventoryTenantId();
     const reqPayLoad = { item_id: productIds };
-    return this.s4sSvc.fetchProductList({ tenantId, body: reqPayLoad});
-  }
-
-  public getContactDetailsOfSelectedSupplier(supplierId : string): Observable<any> {
-    return this.s4sSvc.getContactDetailsOfSelectedSupplier({supplierId} );
-  }
-
-  public getChildItemDetails(childItemId : string): Observable<any> {
-    return this.s4sSvc.getItemDetails({childItemId} )
+    return this.s4sSvc.fetchProductList({ tenantId, body: reqPayLoad })
     .pipe(catchError((err) => observableOf([])));
+  }
+
+  public getContactDetailsOfSelectedSupplier(supplierId: string): Observable<any> {
+    return this.s4sSvc.getContactDetailsOfSelectedSupplier({ supplierId })
+    .pipe(catchError((err) => observableOf([])));
+  }
+
+  public getChildItemDetails(childItemId: string): Observable<any> {
+    return this.s4sSvc.getItemDetails({ childItemId })
+      .pipe(catchError((err) => observableOf([])));
   }
 
   public getByTenantIdV1ConfigurationShipNodes(): Observable<any> {
     const tenantId = BucSvcAngularStaticAppInfoFacadeUtil.getInventoryTenantId();
-    return this.shipNodeSvc.getByTenantIdV1ConfigurationShipNodes({ tenantId } );
+    return this.shipNodeSvc.getByTenantIdV1ConfigurationShipNodes({ tenantId })
+    .pipe(catchError((err) => observableOf([])));
   }
 
   public getShipNodesForSupplier(supplierId: string): Observable<any> {
-    return this.s4sSvc.getShipNodesForSupplier({ supplierId });
+    return this.s4sSvc.getShipNodesForSupplier({ supplierId })
+    .pipe(catchError((err) => observableOf([])));
   }
 }
 
