00d08109088ba12a39e97aa09e03c9d015b736f9
SHISHIR SAHA <ssaha@us.ibm.com>
2020-05-07 15:23:49 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   isGoogleMapEnabled : false,
   S4SAuthorization : 'Basic NDg4NWQ3YzUtMmFiZi00YTQ5LThkYWQtZDE5YjUxOGFjNGEyOkEza1k3ckI3cVkzcUMwa1ExbVAwZ1Q0ZUsxeEUzbVIzbVM3b1A0clYxY0g4ZUw0eVg3',
-  S4SHostPrefix : 'https://api.us-south.apiconnect.appdomain.cloud/nedzhangusibmcom1544551803151-dev/rapid-dev/s4s/'
+  S4SHostPrefix : 'https://api.us-south.apiconnect.appdomain.cloud/nedzhangusibmcom1544551803151-dev/rapid-dev/s4s'
 };
